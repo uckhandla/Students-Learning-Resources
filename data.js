ee57cfdbf95b02d9c6947509546556de2f028829
Ujamshi Khandla <2890171+uckhandla@users.noreply.github.com>
2022-10-04 15:06:03 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const GIST_ID = "e51da4a2753940578e0bb21412bc1895";
-const GIST_FILENAME = "db.json";
+const GIST_FILENAME_1 = "db2.json";
+const GIST_FILENAME_2 = "db.json";
 
 /* 
  * Reads the JSON file inside of the gist
@@ -7,8 +8,12 @@ const GIST_FILENAME = "db.json";
 async function getData() {
   const req = await fetch(`https://api.github.com/gists/${GIST_ID}`);
   const gist = await req.json();
-  return JSON.parse(gist.files[GIST_FILENAME].content);
+  return JSON.parse(gist.files[GIST_FILENAME_1].content);
 }
 
-
+async function getData() {
+  const req = await fetch(`https://api.github.com/gists/${GIST_ID}`);
+  const gist = await req.json();
+  return JSON.parse(gist.files[GIST_FILENAME].content);
+}
 
