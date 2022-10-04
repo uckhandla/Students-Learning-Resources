004f7bfd006df74d57792301b87085f4b27994c5
Ujamshi Khandla <2890171+uckhandla@users.noreply.github.com>
2022-10-04 14:37:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -10,21 +10,5 @@ async function getData() {
   return JSON.parse(gist.files[GIST_FILENAME].content);
 }
 
-async function getDataY() {
-  fetch(`https://api.github.com/gists/${GIST_ID}`).then(function(response) {
-    return response.json();
-  }).then(function(parsedJson) {
-    return parsedJson;
-  })
-}
-
-async function getDataZ() {
-  fetch('https://gist.githubusercontent.com/uckhandla/e51da4a2753940578e0bb21412bc1895/raw/398ee46bf1273084f283f199dfa206f278a1385b/db.json').then(function(response) {
-    return response.json();
-  }).then(function(parsedJson) {
-    return parsedJson;
-  })
-}
-
 
 
