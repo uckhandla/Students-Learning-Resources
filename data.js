fda8d9899c431482bf32258e40665cfa031817c9
Ujamshi Khandla <2890171+uckhandla@users.noreply.github.com>
2022-10-04 11:50:19 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,5 @@
-/*
-const TOKEN = "YOUR_PERSONAL_ACCESS_TOKEN";
-const GIST_ID = "YOUR_GIST_ID";
-const GIST_FILENAME = "db.json";
-*/
-
-const TOKEN = "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";
 const GIST_ID = "e51da4a2753940578e0bb21412bc1895";
 const GIST_FILENAME = "db.json";
-
 
 /* 
  * Reads the JSON file inside of the gist
@@ -18,23 +10,3 @@ async function getData() {
   return JSON.parse(gist.files[GIST_FILENAME].content);
 }
 
-/* 
- * Puts the data you want to store back into the gist
- */
-async function setData(data) {
-  const req = await fetch(`https://api.github.com/gists/${GIST_ID}`, {
-    method: "PATCH",
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-    body: JSON.stringify({
-      files: {
-        [GIST_FILENAME]: {
-          content: JSON.stringify(data),
-        },
-      },
-    }),
-  });
-
-  return req.json();
-}
